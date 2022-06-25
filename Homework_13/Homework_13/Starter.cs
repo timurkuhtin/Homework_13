f56f5c17d934b1471d8e726c0bc7decefb400c26
Tymur Kukhtin <mr.timur.pipal@gmail.com>
2022-06-25 16:57:11 +0300

    Homework_13

---
+++ Homework_13/Homework_13/Starter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_13
+{
+    internal class Starter
+    {
+        public void Go()
+        {
+            var result = Do();
+            Console.WriteLine(result.Result);
+        }
+
+        public async Task<string> ReadHello()
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\Timur\Desktop\A-lavel\Homework\13\Homework_13\Homework_13\txt\Hello.txt"))
+            {
+                await Task.Delay(1000);
+                return reader.ReadLine();
+            }
+        }
+
+        public async Task<string> ReadWorld()
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\Timur\Desktop\A-lavel\Homework\13\Homework_13\Homework_13\txt\World.txt"))
+            {
+                await Task.Delay(1000);
+                return reader.ReadLine();
+            }
+        }
+
+        public async Task<string> Do()
+        {
+            Task<string> first;
+            Task<string> second;
+            await Task.Delay(1000);
+            Task.WaitAll(first = ReadHello(), second = ReadWorld());
+            return first.Result + second.Result;
+        }
+    }
+}
